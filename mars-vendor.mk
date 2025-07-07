709ef8077f61076af39fe5ffca7bda96d0ee36fa
WilliamSado <williamsado1004@gmail.com>
2025-07-08 00:51:06 +0800

    mars: Cleanup fingerprint blobs
    > extension: No longer required
    > dtool: No longer required
    > fod.xml: We include the necessary entries from sm8350-common
    
    Change-Id: I988a09c4c158422260a50f3c7d97e6177096757a

--- mars-vendor.mk
+++ mars-vendor.mk
@@ -182,7 +182,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mars/proprietary/vendor/etc/init/vendor.sensors.qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.sensors.qti.rc \
     vendor/xiaomi/mars/proprietary/vendor/etc/init/vendor.sensors.sscrpcd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.sensors.sscrpcd.rc \
     vendor/xiaomi/mars/proprietary/vendor/etc/init/vendor.xiaomi.hardware.citsensorservice@1.1-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.xiaomi.hardware.citsensorservice@1.1-service.rc \
-    vendor/xiaomi/mars/proprietary/vendor/etc/init/vendor.xiaomi.hardware.dtool@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.xiaomi.hardware.dtool@1.0-service.rc \
     vendor/xiaomi/mars/proprietary/vendor/etc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     vendor/xiaomi/mars/proprietary/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     vendor/xiaomi/mars/proprietary/vendor/etc/ltm_config_xiaomi_38_08_0a_cmd_mode_dsc_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/ltm_config_xiaomi_38_08_0a_cmd_mode_dsc_dsi_panel.xml \
@@ -604,7 +603,6 @@ PRODUCT_PACKAGES += \
     com.xiaomi.plugin.sr \
     com.xiaomi.plugin.supermoon \
     com.xiaomi.plugin.supernight \
-    com.fingerprints.extension@3.0 \
     com.qti.chiusecaseselector \
     com.qti.feature2.anchorsync \
     com.qti.feature2.demux \
@@ -809,7 +807,6 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.campostproc@1.0 \
     vendor.xiaomi.hardware.citsensorservice@1.0 \
     vendor.xiaomi.hardware.citsensorservice@1.1 \
-    vendor.xiaomi.hardware.dtool@1.0 \
     vl53l5.hal@1.0 \
     vl53l5.hal@2.0 \
     vendor_lib_rfsa_adsp_capi_v2_aptX_CLHDADV_Encoder_so \
@@ -858,11 +855,9 @@ PRODUCT_PACKAGES += \
     vendor_lib_rfsa_adsp_misound_res_headphone_bin \
     vendor_lib_rfsa_adsp_misound_res_spk_bin \
     vendor_lib_rfsa_adsp_misound_res_spk_mars_bin \
-    fod.xml \
     vendor.xiaomi.hardware.citsensorservice@1.1-service.xml \
     batterysecret \
     vendor.xiaomi.hardware.citsensorservice@1.1-service \
-    vendor.xiaomi.hardware.dtool@1.0-service \
     init.qcom.sensors \
     sensors.qti \
     sscrpcd
